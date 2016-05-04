fa117bd5c023c79f8c28399067c67e4b920672cd
Sean Cross <xobs@kosagi.com>
2016-05-04 18:16:43 +0800

    app: update syscalls
    They're still a work-in-progress.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- app/syscalls.cpp
+++ app/syscalls.cpp
@@ -1,27 +1,20 @@
 #include "Arduino.h"
 
 extern "C" {
-  void __aeabi_unwind_cpp_pr0(void) {
-    asm("svc #1");
-  }
-  
-  void __aeabi_unwind_cpp_pr1(void) {
-    asm("svc #2");
-  }
 };
 
   void pinMode(int pin, enum pin_mode mode) {
-    asm("svc #3");
+    asm("svc #34");
   }
 
   void digitalWrite(int pin, int value) {
-    asm("svc #4");
+    asm("svc #35");
   }
 
   int digitalRead(int pin) {
-    asm("svc #5");
+    asm("svc #36");
   }
 
   void delay(unsigned long delay) {
-    asm("svc #6");
+    asm("svc #50");
   }
