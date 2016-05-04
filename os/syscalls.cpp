4d96b637e1d7e6af65c9758996a9bb80975e8c29
Sean Cross <xobs@kosagi.com>
2016-05-04 15:54:27 +0800

    os: syscalls: get C intrinsics working
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- os/syscalls.cpp
+++ os/syscalls.cpp
@@ -1,9 +1,14 @@
 #include "Arduino.h"
 
+extern "C" {
+  extern uint32_t __aeabi_unwind_cpp_pr0;
+  extern uint32_t __aeabi_unwind_cpp_pr1;
+};
+
 uint32_t *SysCall_Table[] = {
   0,
-  0,
-  0,
+  (uint32_t *)&__aeabi_unwind_cpp_pr0,
+  (uint32_t *)&__aeabi_unwind_cpp_pr1,
   (uint32_t *)static_cast<void (*)(int, enum pin_mode)>(&pinMode),
   (uint32_t *)static_cast<void (*)(int, int)>(&digitalWrite),
   (uint32_t *)static_cast<int (*)(int)>(&digitalRead),
