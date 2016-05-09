a9a879058a2fbe4a31e170238cb9b3c24b11b26e
Sean Cross <xobs@kosagi.com>
2016-05-09 19:08:10 +0800

    os: update OS infrastructure with more C++, ws2812b
    This adds lots of new OS code.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- os/crt0.c
+++ os/crt0.c
@@ -32,3 +32,17 @@ void __aeabi_unwind_cpp_pr1(void) {
 void __aeabi_unwind_cpp_pr2(void) {
   abort();
 }
+
+char * itoa (int val, char *s, int radix);
+char * ltoa (long val, char *s, int radix) {
+  return itoa(val, s, radix);
+}
+
+char * utoa (unsigned int val, char *s, int radix);
+char * ultoa (unsigned long val, char *s, int radix) {
+  return utoa(val, s, radix);
+}
+/*
+char * ltoa (long val, char *s, int radix)
+  __attribute__((alias("itoa")));
+  */
