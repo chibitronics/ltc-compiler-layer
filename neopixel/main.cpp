fc2bb2f5ffc2d9c24ea1a4798c5d373f578b00f4
Sean Cross <xobs@kosagi.com>
2016-05-17 19:25:36 +0800

    neopixel: add simple test program
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- neopixel/main.cpp
+++ neopixel/main.cpp
@@ -3,7 +3,6 @@
 
 #include "Adafruit_NeoPixel.h"
 
-#if 0
 #define PIN (32 + 3)
 
 // Parameter 1 = number of pixels in strip
@@ -146,21 +145,3 @@ void loop() {
   rainbowCycle(20);
   theaterChaseRainbow(50);
 }
-#else
-
-#define PIN (32 + 10)
-
-void setup(void) {
-  printf("Setting up pin %d\r\n", PIN);
-  pinMode(PIN, OUTPUT);
-  digitalWrite(PIN, LOW);
-}
-
-void loop(void) {
-  printf("Read from pin: %d\r\n", analogRead(PIN));
-  digitalWrite(PIN, LOW);
-  delay(100);
-  digitalWrite(PIN, HIGH);
-  delayMicroseconds(500000);
-}
-#endif
