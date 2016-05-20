f537ca8cfafc26d1912c3abe35e35779777150b6
Sean Cross <xobs@kosagi.com>
2016-05-20 16:37:42 +0800

    flashtest: add simple flash test
    This uses the EEPROM library.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

---
+++ flashtest/main.cpp
@@ -0,0 +1,51 @@
+#include "Arduino.h"
+#include "EEPROM.h"
+
+#if 1
+
+struct MyObject {
+  float field1;
+  byte field2;
+  char name[10];
+};
+
+void setup() {
+
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  float f = 123.456f;  //Variable to store in EEPROM.
+  int eeAddress = 0;   //Location we want the data to be put.
+
+
+  //One simple call, with the address first and the object second.
+  EEPROM.put(eeAddress, f);
+
+  Serial.println("Written float data type!");
+
+  /** Put is designed for use with custom structures also. **/
+
+  //Data to store.
+  MyObject customVar = {
+    3.14f,
+    65,
+    "Working!"
+  };
+
+  eeAddress += sizeof(float); //Move address to the next byte after float 'f'.
+
+  EEPROM.put(eeAddress, customVar);
+  Serial.print("Written custom data type! \n\nView the example sketch eeprom_get to see how you can retrieve the values!");
+}
+
+void loop() {
+  /* Empty loop */
+}
+  #else
+
+  void setup(void) {}
+  void loop(void) {}
+
+  #endif
