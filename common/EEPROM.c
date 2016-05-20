373d596307f51cb043d851da1d10025a64d7b603
Sean Cross <xobs@kosagi.com>
2016-05-20 17:12:56 +0800

    EEPROM: use ChibiOS.h
    Now that we have a ChibiOS header file, use it for EEPROM operations.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- common/EEPROM.c
+++ common/EEPROM.c
@@ -1,24 +1,10 @@
 #include <string.h>
 
 #include "Arduino.h"
-#ifndef E2END
-#define E2END 511
-#endif
+#include "ChibiOS.h"
+#include "EEPROM.h"
 
-typedef struct {
-  uint32_t data[5];
-} virtual_timer_t;
-typedef uint32_t systime_t;
-typedef void (*vtfunc_t)(void *p);
-
-virtual_timer_t my_timer;
-
-extern void setTimer(virtual_timer_t *vtp, systime_t delay,
-                     vtfunc_t vtfunc, void *par);
-extern void unlockSystemFromISR(void);
-extern void lockSystemFromISR(void);
-extern int8_t flashErase(uint32_t sectorOffset, uint16_t sectorCount);
-extern int8_t flashWrite(uint8_t *src, const uint8_t *dst, uint32_t count);
+static virtual_timer_t my_timer;
 
 __attribute__((section(".flasheeprom")))
 static const uint8_t *flash_area;
@@ -55,7 +41,7 @@ uint8_t eeprom_write_byte(uint8_t *index, uint8_t val) {
   flash_backing[(uint32_t)index] = val;
   flash_state = STATE_DIRTY;
 
-  setTimer(&my_timer, 1000, eeprom_flush, NULL);
+  setTimer(&my_timer, MS2ST(30), eeprom_flush, NULL);
 
   return 0;
 }
