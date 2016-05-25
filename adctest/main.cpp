aa514dcb7441b4b8323e1805277197253f654c97
Sean Cross <xobs@kosagi.com>
2016-05-25 21:23:42 +0800

    adctest: go back to the really useful printout
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- adctest/main.cpp
+++ adctest/main.cpp
@@ -1,17 +1,36 @@
 #include "Arduino.h"
 
-#define PIN (32 + 10)
-
 void setup(void) {
-  printf("Setting up pin %d\r\n", PIN);
-  pinMode(PIN, OUTPUT);
-  digitalWrite(PIN, LOW);
+  /*
+  analogWrite(A0, 500);
+  analogWrite(A1, 250);
+  analogWrite(A2, 200);
+  analogWrite(A3, 1020);
+  */
 }
 
 void loop(void) {
-  printf("Read from pin: %d\r\n", analogRead(PIN));
-  digitalWrite(PIN, LOW);
-  delay(100);
-  digitalWrite(PIN, HIGH);
-  delayMicroseconds(500000);
+  /*
+  static int i;
+
+  analogWrite(A0, (i/1 + 500) & 0x3ff);
+  analogWrite(A1, (i/1 + 250) & 0x3ff);
+  analogWrite(A2, (i/1 + 200) & 0x3ff);
+  analogWrite(A3, (i/1 + 1020) & 0x3ff);
+
+  i++;
+  */
+
+  printf("\r\nReading values...\r\n");
+  printf("A0: %d\r\n", analogRead(A0));
+  printf("A1: %d\r\n", analogRead(A1));
+  printf("A2: %d\r\n", analogRead(A2));
+  printf("A3: %d\r\n", analogRead(A3));
+  printf("A4: %d\r\n", analogRead(A4));
+  printf("A5: %d\r\n", analogRead(A5));
+  printf("A6: %d\r\n", analogRead(A6));
+  printf("A7: %d\r\n", analogRead(A7));
+  printf("A8: %d\r\n", analogRead(A8));
+
+  delayMicroseconds(50000);
 }
