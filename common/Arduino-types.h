c4a93dfe8d182bd10d51e8d189f6f9b09680dacb
Sean Cross <xobs@kosagi.com>
2016-05-25 22:58:10 +0800

    Arduino-types: add [u]int64_t
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- common/Arduino-types.h
+++ common/Arduino-types.h
@@ -10,6 +10,8 @@ typedef unsigned char uint8_t;
 typedef signed char int8_t;
 typedef unsigned short uint16_t;
 typedef signed short int16_t;
+typedef long long int64_t;
+typedef unsigned long long uint64_t;
 
 typedef uint32_t size_t;
 
