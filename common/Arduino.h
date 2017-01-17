1d55797df5f20467102f5d528bb1c09d1c8f2685
Sean Cross <sean@xobs.io>
2017-01-17 21:53:30 +0800

    Arduino: add map() function
    This function was removed from the OS.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- common/Arduino.h
+++ common/Arduino.h
@@ -222,6 +222,10 @@ static inline long random(long max) {
 #endif
 long randomSeed(unsigned long seed);
 
+static inline long map(long x, long in_min, long in_max, long out_min, long out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 /* Bits and Bytes */
 #define lowByte(w) ((uint8_t) ((w) & 0xff))
 #define highByte(w) ((uint8_t) ((w) >> 8))
