7580e798e2139dc1694e8c870f2974f632073de0
Sean Cross <sean@xobs.io>
2017-03-31 12:09:54 +0800

    ltctest: add status led test
    Signed-off-by: Sean Cross <sean@xobs.io>

---
+++ ltctest/04-status-leds.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+source ./00-test-lib.sh
+
+echo "Status LEDs:"
+
+echo "   Green On"
+wait_for_green_on
+echo 'g' > ${uart}
+
+echo "    Green Off"
+wait_for_green_off
+echo 'g' > ${uart}
+
+echo "    Red Off"
+wait_for_red_off
+echo 'r' > ${uart}
+
+echo "    Red On"
+wait_for_red_on
+echo 'r' > ${uart}
