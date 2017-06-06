5cc5250fae3e11c9023151a54910669b103adbc6
Sean Cross <sean@xobs.io>
2017-06-06 13:38:16 +0800

    gen-syscalls: add a type for C-with-prototypes
    We would like to generate valid support files, and as such need more
    information.
    
    Support a new type of syscall - P.  These are simply C functions, but
    include additional information such as return type and function
    arguments.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- support/gen-syscalls.pl
+++ support/gen-syscalls.pl
@@ -6,6 +6,7 @@ sub generate_files {
   my ($syscalls, $os_fh, $app_fh) = @_;
 
   my @c_syscalls;
+  my @c_ext_syscalls;
   my @cxx_methods;
   my @cxx_member_methods;
   my @cxx_constructors;
@@ -44,6 +45,29 @@ sub generate_files {
       }
       $idx++;
     }
+    elsif ($call->[1] eq "P") {
+      my ($call_name) = ($call->[2]);
+      if (defined($call->[5])) {
+        my @subcalls = split(/\s*,\s*/, $call->[5]);
+
+        # Add an alias for the syscall name, to avoid confusion
+        if (!grep(/^$call_name$/, @subcalls)) {
+          push(@subcalls, $call_name);
+        }
+
+        # For each alias, add it to the list of C calls
+        for my $sc(@subcalls) {
+          my @new_call = @$call;
+          $new_call[5] = $sc;
+          push (@c_ext_syscalls, \@new_call);
+        }
+      }
+      else {
+        $call->[5] = $call->[2];
+        push (@c_ext_syscalls, $call);
+      }
+      $idx++;
+    }
 
     if ($call->[1] eq "x") {
       push (@cxx_methods, $call);
@@ -60,17 +84,6 @@ sub generate_files {
   }
 
   print $app_fh "#include \"Arduino-types.h\"\n";
-
-#  if (@c_syscalls) {
-#    open(my $test_fh, '>', "eabi-lookup.c");
-#    for my $c_syscall(@c_syscalls) {
-#      my ($idx, $code, $sysname, $name) = @$c_syscall;
-#      next unless $sysname =~ /qfp_/;
-#      print $test_fh "extern float $sysname(void);\n";
-#      print $test_fh "float $name(void) { return $sysname(); }\n";
-#    }
-#    close($test_fh);
-#  }
 
   print $os_fh ".section .rodata\n";
   print $os_fh ".global SysCall_Table\n";
@@ -97,6 +110,23 @@ sub generate_files {
     print $app_fh "};\n";
   }
 
+  if (@c_ext_syscalls) {
+    my %seen_c_syscalls;
+
+    print $app_fh "extern \"C\" {\n";
+    for my $c_syscall(@c_ext_syscalls) {
+      my ($idx, $code, $sysname, $rettype, $args, $name) = @$c_syscall;
+
+      $table[$idx] = $c_syscall;
+
+      print $app_fh "    __attribute__((naked))\n";
+      print $app_fh "    $rettype $name($args) {\n";
+      print $app_fh "      asm(\"svc #$idx\");\n";
+      print $app_fh "    }\n";
+    }
+    print $app_fh "};\n";
+  }
+
   for my $cxx_method(@cxx_methods) {
     my ($idx, $code, $name, $rettype, $args, $names) = @$cxx_method;
 
@@ -114,34 +144,31 @@ sub generate_files {
   print $os_fh "\n";
 
   # Generate the syscall table
-  # TODO: Remove this __attribute__
-#  print $os_fh "__attribute__((section(\".rodata\")))\n";
-#  print $os_fh "const uint32_t * const SysCall_Table[] = {\n";
 
   for my $i (0 .. @table-1) {
     my $syscall = $table[$i];
     if (!defined($syscall)) {
       print $os_fh ".short 0\n";
-#      print $os_fh "    (const uint32_t *)0,\n";
     }
     elsif ($syscall->[1] eq "c") {
       my ($idx, $code, $sysname, $name) = @$syscall;
       $sysname = $name if (!defined($sysname));
       print $os_fh ".short $sysname\n";
-#      print $os_fh "    (const uint32_t *)&$sysname,\n";
+    }
+    elsif ($syscall->[1] eq "P") {
+      my ($idx, $code, $sysname, $rettype, $args, $name) = @$syscall;
+      $sysname = $name if (!defined($sysname));
+      print $os_fh ".short $sysname\n";
     }
     elsif ($syscall->[1] eq "x") {
       my ($idx, $code, $name, $rettype, $args, $names) = @$syscall;
       print $os_fh ".short $name\n";
-#      print $os_fh "    (const uint32_t *)&$name\n";
     }
     else {
       print $os_fh ".short 0\n";
-#      print $os_fh "    (const uint32_t *)0,\n";
     }
   }
   print $os_fh ".size SysCall_Table, .-SysCall_Table\n";
-#  print $os_fh "};\n";
 }
 
 my @syscalls;
